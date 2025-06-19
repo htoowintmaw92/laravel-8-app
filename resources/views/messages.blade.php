536c767a60b09971dc686cc91e6ec7eec308f3e4
hhtoo <kg-htoo@infonic.co.jp>
2025-06-20 04:24:14 +0700

    Update messages.blade.php

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -30,7 +30,7 @@
         window.Echo = new Echo({
             broadcaster: 'pusher',
             key: "{{ config('broadcasting.connections.pusher.key') }}",
-            wsHost: "{{ config('broadcasting.connections.pusher.options.host') }}" || window.location.hostname,
+            wsHost: window.location.hostname,
             wsPort: "{{ config('broadcasting.connections.pusher.options.port', 6001) }}",
             forceTLS: false,
             disableStats: true,
