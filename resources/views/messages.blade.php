85d0ccf39b6c427939481c4f2ab149a38ec30797
hhtoo <kg-htoo@infonic.co.jp>
2025-06-19 17:04:50 +0700

    Revert "refs #develop"
    This reverts commit a1109ccca36629f64eb714edbdcfe7506b9ffb91.

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -30,7 +30,7 @@
         window.Echo = new Echo({
             broadcaster: 'pusher',
             key: "localkey",
-            wsHost: "{{ config('broadcasting.connections.pusher.options.host', request()->getHost()) }}",
+            wsHost: "192.168.10.10",
             wsPort: "{{ config('broadcasting.connections.pusher.options.port', 6001) }}",
             forceTLS: false,
             disableStats: true,
