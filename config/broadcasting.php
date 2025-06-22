20c0264f6440c9f7749e271f36547f4c027cb044
hhtoo <kg-htoo@infonic.co.jp>
2025-06-23 03:57:12 +0700

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,9 +39,9 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
                 // 'useTLS' => true,
-                // 'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
-                // 'port' => env('PUSHER_APP_PORT', 6001),
-                // 'scheme' => env('PUSHER_APP_SCHEME', 'http'),
+                'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_APP_PORT', 6001),
+                'scheme' => env('PUSHER_APP_SCHEME', 'http'),
             ],
         ],
 
