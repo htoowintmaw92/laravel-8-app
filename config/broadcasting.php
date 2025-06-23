c43f88974a30cd28f75b4822e251f261fb2da04c
hhtoo <kg-htoo@infonic.co.jp>
2025-06-23 12:42:02 +0700

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -44,10 +44,10 @@ return [
                 'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_APP_PORT', 6001),
                 'scheme' => env('PUSHER_APP_SCHEME', 'http'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+                // 'curl_options' => [
+                //     CURLOPT_SSL_VERIFYHOST => 0,
+                //     CURLOPT_SSL_VERIFYPEER => 0,
+                // ],
             ],
         ],
 
